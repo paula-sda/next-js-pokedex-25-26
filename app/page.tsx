c0f0a25d72f854636a74d931687e7ffe670badd9
paula.saenzdeargandona <paula.saenzdeargandona@gmail.com>
2026-01-01 09:57:43 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,9 @@ export default async function Home({ searchParams }: PokemonListPageProps) {
   const pokemonData = await getPokemonList(currentPage);
   const pokemonList = pokemonData?.results || [];
 
-  const totalPokemon = pokemonData?.count || 0;
+  const MAX_POKEMON = 898;
+
+  const totalPokemon = Math.min(pokemonData?.count || 0, MAX_POKEMON);
   const totalPages = Math.ceil(totalPokemon / pokemonPerPage);
 
   return (
