b0ef64987e6f7b90ff566e646d7a648991fc863a
PAULA SÁENZ DE ARGANDOÑA <166641473+paula-sda@users.noreply.github.com>
2026-01-05 17:36:54 +0100

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="w-full flex p-10 justify-center items-center bg-gradient-to-r from-orange-400 via-cyan-500 to-purple-600">
+        <header className="w-full flex p-10 justify-center items-center bg-gradient-to-r from-pink-400 via-cyan-500 to-purple-600">
             <Link className="text-3xl font-bold mb-4 text-center text-white" href="/">PokéDex</Link>
         </header>
     )
